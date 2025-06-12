617517523f966b7c4f0371b033478cd4fccba2b3
francesjgonzales <francesj7837@gmail.com>
2025-06-12 15:56:09 -0600

    added render link in allowed hosts

--- newsoleproj/settings.py
+++ newsoleproj/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-svk576%-o!tcgjqa(2r!$c)nq-(-p%cb&!l(8&1!kl@l=x@pk1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['newsole-proj-django.onrender.com']
 
 
 # Application definition
