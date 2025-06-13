b00e3daf8c48b654cf41b5e450a19779bcef2eeb
francesjgonzales <francesj7837@gmail.com>
2025-06-13 17:45:40 -0600

    changes in settings.py

--- newsoleproj/settings.py
+++ newsoleproj/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-svk576%-o!tcgjqa(2r!$c)nq-(-p%cb&!l(8&1!kl@l=x@pk1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['newsole-proj-django.onrender.com', 'localhost', '127.0.0.1']
 
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
