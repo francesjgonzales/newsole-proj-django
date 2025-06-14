aca517b87648cb749eafd817c461114f6af51762
francesjgonzales <francesj7837@gmail.com>
2025-06-14 11:23:26 -0600

    collectstatic

--- newsoleapp/urls.py
+++ newsoleapp/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -11,4 +10,3 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
