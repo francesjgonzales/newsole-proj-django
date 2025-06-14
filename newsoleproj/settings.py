21da5bfe77db5b4bddf1e8d1a6987cf9dbeefc3d
francesjgonzales <francesj7837@gmail.com>
2025-06-14 11:09:14 -0600

    fixing media files

--- newsoleproj/settings.py
+++ newsoleproj/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-svk576%-o!tcgjqa(2r!$c)nq-(-p%cb&!l(8&1!kl@l=x@pk1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['newsole-proj-django.onrender.com', 'localhost', '127.0.0.1']
 
@@ -136,9 +136,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = ''
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '')
 
 # Default primary key field type
